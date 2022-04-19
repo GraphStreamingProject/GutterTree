3e40f9e7832c02637cb625e9cbd010e1685e3518
Evan West <etwest@cs.stonybrook.edu>
2022-04-19 16:23:36 -0400

    cache guttering experiment

---
+++ experiment/cache_exp.cpp
@@ -0,0 +1,98 @@
+#include <gtest/gtest.h>
+#include <thread>
+#include <chrono>
+#include <atomic>
+#include <fstream>
+#include "../include/cache_guttering.h"
+
+static bool shutdown = false;
+
+// queries the guttering system
+// Should be run in a seperate thread
+static void querier(GutteringSystem *gts) {
+  WorkQueue::DataNode *data;
+  while(true) {
+    bool valid = gts->get_data(data);
+    if(!valid && shutdown)
+      return;
+    gts->get_data_callback(data);
+  }
+}
+
+static void write_configuration(int queue_factor, int gutter_factor) {
+  std::ofstream conf("./buffering.conf");
+  conf << "queue_factor=" << queue_factor << std::endl;
+  conf << "gutter_factor=" << gutter_factor << std::endl;
+}
+
+static void run_test(const int nodes, const unsigned long updates, const unsigned int nthreads=1) {
+  shutdown = false;
+  write_configuration(2, 1); // 2 is queue_factor, 1 is gutter_factor
+  CacheGuttering *gutters = new CacheGuttering(nodes, 40, nthreads); // 40 is num workers
+
+  // create queriers
+  std::thread query_threads[40];
+  for (int t = 0; t < 40; t++) {
+    query_threads[t] = std::thread(querier, gutters);
+  }
+
+  std::vector<std::thread> threads;
+  threads.reserve(nthreads);
+  const unsigned long work_per = (updates + (nthreads-1)) / nthreads;
+  printf("work per thread: %lu\n", work_per);
+
+  auto task = [&](const unsigned int j){
+    for (unsigned long i = j * work_per; i < (j+1) * work_per && i < updates; i++) 
+    {
+      if(i % 1000000000 == 0)
+        printf("processed so far: %lu\n", i);
+      update_t upd;
+      upd.first = i % nodes;
+      upd.second = (nodes - 1) - (i % nodes);
+      gutters->insert(upd, j);
+      std::swap(upd.first, upd.second);
+      gutters->insert(upd, j);
+    }
+  };
+
+  auto start = std::chrono::steady_clock::now();
+  //Spin up then join threads
+  for (unsigned int j = 0; j < nthreads; j++)
+    threads.emplace_back(task, j);
+  for (unsigned int j = 0; j < nthreads; j++)
+    threads[j].join();
+
+  gutters->force_flush();
+  shutdown = true;
+  gutters->set_non_block(true); // switch to non-blocking calls in an effort to exit
+  
+  std::chrono::duration<double> delta = std::chrono::steady_clock::now() - start;
+  printf("Insertions took %f seconds: average rate = %f\n", delta.count(), updates/delta.count());
+
+  for (int t = 0; t < 40; t++)
+    query_threads[t].join();
+  delete gutters;
+}
+
+TEST(CG_Throughput, kron15_10threads) {
+  run_test(8192, 17542263, 10);
+}
+
+TEST(CG_Throughput, kron15_20threads) {
+  run_test(8192, 17542263, 20);
+}
+
+TEST(CG_Throughput, kron17_10threads) {
+  run_test(131072, 4474931789, 10);
+}
+
+TEST(CG_Throughput, kron17_20threads) {
+  run_test(131072, 4474931789, 20);
+}
+
+TEST(CG_Throughput, kron18_10threads) {
+  run_test(262144, 17891985703, 10);
+}
+TEST(CG_Throughput, kron18_20threads) {
+  run_test(262144, 17891985703, 20);
+}
